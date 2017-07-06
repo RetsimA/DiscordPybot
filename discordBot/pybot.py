2defcc82e51c8b7dab07ba64cb6c66ca2e478a0d
Aurob <robau6@gmail.com>
2017-07-05 19:55:22 -0500

    Add files via upload

--- discordBot/pybot.py
+++ discordBot/pybot.py
@@ -12,34 +12,121 @@ import re
 
 
 client= discord.Client()
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-#ANY TIME A USER SENDS A MESSAGE------------------------------------------------------------------------------------
 @client.event #THIS LINE AND ASYNC ON THE NEXT LINE ARE REQUIRE FOR FUNCTIONS
+#ANY TIME A USER SENDS A MESSAGE
 async def on_message(message):
     
     msg= (message.content).lower()
     user= message.author.name
+    ch= message.channel
 
-    if msg.startswith("/apod"):
-            inp= msg[6:]
+    if msg.startswith('/sp'):
+        await client.delete_message(message)
+        inp= msg[4:]
+        inp= ' '.join(inp)
+        await client.send_message(ch, inp)
+        
+    if msg.startswith('/img'):
+        try:
+            inp= msg[5:]
+            link= 'https://www.flickr.com/search/?advanced=1&dimension_search_mode=min&height=1920&width=1080&text=',inp
+            print(''.join(link))
+            soup3 = requests.get(''.join(link))
+            soup= BeautifulSoup(soup3.content,"html.parser")
 
+            b= str(soup.find('div',{'class' : 'view photo-list-photo-view requiredToShowOnServer awake'}))
+            b= b.split("url(//")[1].split(')">')[0]
+            b='https://',b
+            print(''.join(b))
+            em= discord.Embed(title=inp, colour=0x48437)
+            emImg= discord.Embed.set_image(em,url=''.join(b))
+            await client.send_message(ch, embed=em)
+        except:
             
-            link= 'https://apod.nasa.gov/apod/ap',inp,'.html'
+            await client.send_message(ch, "Try another search input.")
+        
+    #DEFINITION___________
+    if msg.startswith("/define"):
+        try:
+            await client.delete_message(message)
+            inp= msg[8:]
+            link= 'http://www.dictionary.com/browse/',inp
             print(''.join(link))
             soup3 = requests.get(''.join(link))
             soup= BeautifulSoup(soup3.content,"lxml")
-            img= str(soup.findAll('a',{"href":True}))
-            imglink= img.split('src="')[1].split('"/>')[0]
-            imggoto= "https://apod.nasa.gov/apod/",imglink
-            imggoto= "".join(imggoto)
-            imggoto= imggoto.replace(" ", "")
-            
-            imggoto= imggoto.replace('"style="max-width:100%', "")
-            print(imggoto)
-            await client.send_message(message.channel, imggoto)
-            
+
+            b= soup.find('span',{'class' : 'dbox-pg'})
+            b= ' '.join(b.get_text().split())
+            b=b.replace('. ', '.\n')
+
+            a= soup.find('div',{'class' : 'def-content'})
+            a= ' '.join(a.get_text().split())
+            a=a.replace('. ', '.\n')
+
+            word= inp,b
+            em= discord.Embed(title= '- '.join(word) , description=a, colour=0xABCDE)
+            await client.send_message(ch, embed=em)
+        except:
+            await client.send_message(ch, "No definition of {0}".format(inp))
+
+    #ASTRONOMY PICTURE OF THE DAY____________       
+    if msg.startswith("/apod"):
+        inp= msg[6:]
+        if len(inp)<6 or len(inp)>6:
+            await client.send_message(ch, "Incorrect format; /apod YYMMDD")
+            await client.delete_message(message)
+        else:
+            if inp.isdigit():
+                try:
+                    link= 'https://apod.nasa.gov/apod/ap',inp,'.html'
+                    print(''.join(link))
+                    soup3 = requests.get(''.join(link))
+                    soup= BeautifulSoup(soup3.content,"lxml")
+                    img= str(soup.findAll('a',{"href":True}))
+                    imglink= img.split('src="')[1].split('"/>')[0]
+                    imggoto= "https://apod.nasa.gov/apod/",imglink
+                    imggoto= "".join(imggoto)
+                    imggoto= imggoto.replace(" ", "")     
+                    imggoto= imggoto.replace('"style="max-width:100%', "")
+                    
+                    do= soup.find_all('p')
+                    do2= str(do[2])
+                    do2= re.sub('<[^>]+>', '', do2)
+                    do2= do2.split()
+                    do2= " ".join(do2)
+                    do2= do2.replace(". ",".\n\n")
+                    do2= do2.replace("? ",".\n\n")
+                    do2= do2.replace("! ",".\n\n")
+                    do2= do2.replace("Explanation: ","")
+
+                    
+                    em= discord.Embed(description=do2, colour=0x48437)
+                    emImg= discord.Embed.set_image(em,url=imggoto)                    
+
+                    await client.send_message(ch, embed=em)
+                    
+                except IndexError:
+                    
+                    client.send_message(ch, 'Incorrect format; /apod YYMMDD')
+                    await client.delete_message(message)
+                    
+            elif not inp.isdigit():
+                
+                await client.send_message(ch, "Incorrect format; /apod YYMMDD")
+                await client.delete_message(message)
+                
+    #RELIGIOUS TEXT___________________              
     if msg.startswith("/verse"):
         inp= msg[7:]
+
+        #BIBLE______________
         if inp.lower() == "bible":
             
             await client.delete_message(message)
@@ -49,125 +136,32 @@ async def on_message(message):
             verse= str(soup.select('.bibleVerse'))
             verse= verse.split('[<div class="bibleVerse">')[1].split('<div class')[0]
             verse= verse.replace("<br/>"," ")
-            verse= verse.replace("God", user)
-            verse= verse.replace("Jesus", user)
-            verse= verse.replace("Lord", user)
-            verse= verse.replace("lord", user)
-            verse= verse.replace("jesus", user)
 
             verse2= str(soup.select('.bibleChapter'))
             verse2= re.sub('<[^>]+>', '', verse2)
             verse2= verse2.split('[')[1].split('|')[0]
-            em= discord.Embed(title= verse , description= verse2, colour=0x48437)
-            await client.send_message(message.channel, embed=em)
             
+            em= discord.Embed(title= verse , description= verse2, colour=0x48437)
+            await client.send_message(ch, embed=em)
+            
+        #QURAN____________    
         elif inp.lower() == 'quran':
             
             await client.delete_message(message)
             req= requests.get('http://ayatalquran.com/random')
             soup= BeautifulSoup(req.content,"html.parser")
+            
             verse= str(soup.select('#aya_text'))
             verse= verse.split('[<h2 id="aya_text">')[1].split('</h2>]')[0]
 
             book1= str(soup.select('#sura_id'))
             book1= book1.split('[<span id="sura_id">')[1].split('</span>]')[0]
+            
             book2= str(soup.select('#verse_id'))
             book2= book2.split('[<span id="verse_id">')[1].split('</span>]')[0]
-
             book= "The Holy Quran ",book1,":",book2
+            
             em= discord.Embed(title= verse , description="".join(book), colour=0x48437)
-            await client.send_message(message.channel, embed=em)
-            
-            
-words= ['cock','pussy','fuck','shit','damn','hell','cunt','bitch','ass', 'nazi','nigger','nigga','jap','gook','zipperface','wigger','skinhead','fag','twat','dammit','nig ']
-phrases= ["this is a friendly progressive channel, please don't use that kind of deprecated language.",
-          "please dont speak like that, we are a Christian channel",
-          "don't use that kind of language, please.",
-          "do I need to bring out the soap?"]
-languages= ["arabic","hebrew","haitian creole","greek","german",
-            "french","finnish","estonian","dutch","danish",
-            "czech","catalan","bulgarian","hindi",
-            "urdu","ukranian","turksih","thai","swedish",
-            "spanish","slovenian","slovak","russian","romanian",
-            "portuguese","polish","persian","norwegian",
-            "maltese","malay","lithuanian","latvian","korean",
-            "klingon","japanese","italian","indonesian","hungarian"]
-            
-users= []
-#languages= open("languages.txt","r")
-
-#REQUIRED TO START BOT --------------------------------------------------
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+            await client.send_message(ch, embed=em)
     
 client.run('MzI4MzExOTgxMTg4ODQxNDgy.DDRtOQ.abeJ3CSJ9k5XoHHxWk6ZkfOjK0U')
-#------------------------------------------------------------------------
-
-#Translate function------------------------------------------------------------------------        
-'''
-if msg.startswith("/tr"):
-    
-    if msg.startswith("/trlist"):
-        await client.send_message(message.channel, "\n".join(languages))
-        
-    else:
-        inp= msg[4:] #REMOVES THE FIRST 4 CHARACTERS OF THE STRING; ie. REMOVES /tr FROM USER INPUT
-        
-        if ";" not in inp:
-            await client.send_message(message.channel, 'Incorrect Format. Correct format: \n/tr "language" ; "text to be translated"')
-
-        #REMINDER! ADD CHECK FOR MULTIPLE SEMICOLONS    
-        else:
-            #THESE TWO LINES SEPERATE THE INPUT INTO A STRING ARRAY WITH THE SEMICOLON AS THE IDENTIFIER OF WHERE TO SPLIT
-            #0 AND 1 DESCRIBE THE LEFT AND RIGHT SIDE OF THE SEMICOLON
-            inpLang= inp.split(';')[0]
-            inpText= inp.split(';')[1]
-
-            #THIS IF CHECKS IF THE USER INPUT IS EMPTY OR IF IT IS EMPTY SPACES
-            if inpText.isspace() or not inpText:
-                await client.send_message(message.channel, 'Text to translate cannot be blank.')
-                
-            else:    
-                for a in range(38):
-                    if languages[a] in inpLang:
-                        print(inpText,languages[a],sep= ' : ')
-                        await client.delete_message(message)
-                        
-                        await translate(user, languages[a], inpText, message.channel) #CALLS THE TRANSLATE FUNCTION WHICH ACITVATES SELENIUM
-                        break
-                    
-                    #IF THE FOR LOOP REACHES THE END, THE INPUT LANGUAGE WAS NOT FOUND
-                    if a == 37:
-                        await client.send_message(message.channel, "{0} is not an available language.\nType /trhelp to see a list of available languages.".format(inpLang))
-                        
-async def translate(user,language,text,channel):
-    
-    tmp= await client.send_message(channel, "Loading translation of '{0}'".format(text))
-    
-    url= "http://translate.reference.com/english/",language.strip(),"/",text
-    browser= webdriver.Firefox()
-    browser.get("".join(url))
-    
-    translated= browser.find_element_by_id("clipboard-text").text
-    await client.edit_message(tmp, "Translated!")
-
-    string= user,",",'"',text,'"'," in ", language, " is..."
-    em= discord.Embed(title= " ".join(string) , description=translated, colour=0xFE34A)
-    await client.send_message(channel, embed=em)
-    browser.quit()
-    
-    return str(translated)#RETURNS THE TRANSLATED TEXT
-'''
-#PROFANITY FILTER; UNDER CONSTRUCTION
-'''
-    for a in range(21):
-        if words[a] in msg:
-            em= discord.Embed(title= user, description=phrases[random.randint(0,3)], colour=0xFE34A)
-            await client.send_message(message.channel, embed=em)
-            userCuss(user)
-            break
-'''
